38cd66099dd4c46eb3a656d670bb09616e1fc134
Narendras <n.syahrasyad.n@gmail.com>
2019-08-03 23:57:40 -0700

    Init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Downshift from 'downshift'
+import clsx from 'clsx'
+import Spotlight from '@narendras/spotlight'
+
+import icon from './icon.svg'
+import './App.scss'
 
 function App() {
+  const [value, setValue] = React.useState('')
+  const inputRef = React.createRef()
+
+  const spt = new Spotlight(['orange', 'melon', 'watermelon'])
+  const results = spt.getResults(value)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className="main">
+      <Downshift inputValue={value} onInputValueChange={val => setValue(val)}>
+        {utils => (
+          <div className="spotlight">
+            <div
+              className={clsx('input_container', {
+                'input_container--active': utils.isOpen && results.length > 0
+              })}
+            >
+              <img src={icon} alt="spotlight icon" className="input_icon" />
+              <input
+                className="input"
+                ref={inputRef}
+                {...utils.getInputProps({ placeholder: 'Spotlight Search' })}
+              />
+            </div>
+            <ul {...utils.getMenuProps()}>
+              {utils.isOpen
+                ? results.map(result => (
+                    <li {...utils.getItemProps({ key: result, item: result })}>
+                      {result}
+                    </li>
+                  ))
+                : null}
+            </ul>
+          </div>
+        )}
+      </Downshift>
+    </main>
+  )
 }
 
-export default App;
+export default App
